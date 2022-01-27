95027827766de65270cb6b45c5648597aa5a8784
Ramid Khan <ramidzkh@gmail.com>
2022-01-27 16:34:25 +1100

    Fix chemical rendering

--- src/main/java/me/ramidzkh/mekae2/AChemicalStackRenderer.java
+++ src/main/java/me/ramidzkh/mekae2/AChemicalStackRenderer.java
@@ -2,11 +2,15 @@ package me.ramidzkh.mekae2;
 
 import appeng.api.client.AEStackRendering;
 import appeng.api.client.IAEStackRenderHandler;
+import appeng.client.gui.style.Blitter;
 import com.mojang.blaze3d.vertex.PoseStack;
 import me.ramidzkh.mekae2.ae2.MekanismKey;
 import me.ramidzkh.mekae2.ae2.MekanismKeyType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -24,12 +28,68 @@ public class AChemicalStackRenderer<T extends MekanismKey<?>> implements IAEStac
 
     @Override
     public void drawInGui(Minecraft minecraft, PoseStack poseStack, int x, int y, int zIndex, T what) {
-        minecraft.font.drawShadow(poseStack, what.getStack().getTextComponent(), x, y, 0xFF | what.getStack().getType().getColorRepresentation());
+        var stack = what.getStack();
+
+        Blitter.sprite(Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(stack.getType().getIcon()))
+                .colorRgb(stack.getChemicalTint())
+                // Most fluid texture have transparency, but we want an opaque slot
+                .blending(false)
+                .dest(x, y, 16, 16)
+                .blit(poseStack, 100 + zIndex);
     }
 
     @Override
     public void drawOnBlockFace(PoseStack poseStack, MultiBufferSource buffers, T what, float scale, int combinedLight) {
-        Minecraft.getInstance().font.drawInBatch(what.getStack().getTextComponent(), 0, 0, 0xFF | what.getStack().getType().getColorRepresentation(), true, poseStack.last().pose(), buffers, false, 0, combinedLight);
+        var stack = what.getStack();
+        var sprite = Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(stack.getType().getIcon());
+        var color = stack.getChemicalTint();
+
+        poseStack.pushPose();
+        // Push it out of the block face a bit to avoid z-fighting
+        poseStack.translate(0, 0, 0.01f);
+
+        var buffer = buffers.getBuffer(RenderType.solid());
+
+        // In comparison to items, make it _slightly_ smaller because item icons
+        // usually don't extend to the full size.
+        scale -= 0.05f;
+
+        // y is flipped here
+        var x0 = -scale / 2;
+        var y0 = scale / 2;
+        var x1 = scale / 2;
+        var y1 = -scale / 2;
+
+        var transform = poseStack.last().pose();
+        buffer.vertex(transform, x0, y1, 0)
+                .color(color)
+                .uv(sprite.getU0(), sprite.getV1())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
+        buffer.vertex(transform, x1, y1, 0)
+                .color(color)
+                .uv(sprite.getU1(), sprite.getV1())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
+        buffer.vertex(transform, x1, y0, 0)
+                .color(color)
+                .uv(sprite.getU1(), sprite.getV0())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
+        buffer.vertex(transform, x0, y0, 0)
+                .color(color)
+                .uv(sprite.getU0(), sprite.getV0())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
+        poseStack.popPose();
     }
 
     @Override
