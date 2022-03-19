62420e192c1b1017792ad162efb98eb21f8c4abd
Ramid Khan <ramidzkh@gmail.com>
2022-03-19 11:59:23 +1100

    New textures
    Co-Authored-By: ProjectET <projectet@hotmail.com>

--- src/main/java/me/ramidzkh/mekae2/data/ItemModelProvider.java
+++ src/main/java/me/ramidzkh/mekae2/data/ItemModelProvider.java
@@ -38,7 +38,8 @@ public class ItemModelProvider extends net.minecraftforge.client.model.generator
         for (var tier : AItems.Tier.values()) {
             var cell = AItems.get(tier);
             var portableCell = AItems.getPortableCell(tier);
-            cell(cell, portableCell, "item/" + cell.getId().getPath());
+            cell(cell, "item/" + cell.getId().getPath());
+            portableCell(portableCell, "item/portable_" + cell.getId().getPath());
         }
 
         withExistingParent("item/chemical_p2p_tunnel", P2P_TUNNEL_BASE_ITEM)
@@ -47,9 +48,12 @@ public class ItemModelProvider extends net.minecraftforge.client.model.generator
                 .texture("type", OSMIUM_BLOCK);
     }
 
-    private void cell(RegistryObject<Item> cell, RegistryObject<Item> portable, String background) {
+    private void cell(RegistryObject<Item> cell, String background) {
         singleTexture(cell.getId().getPath(), mcLoc("item/generated"), "layer0", AE2MekanismAddons.id(background))
                 .texture("layer1", STORAGE_CELL_LED);
+    }
+
+    private void portableCell(RegistryObject<Item> portable, String background) {
         singleTexture(portable.getId().getPath(), mcLoc("item/generated"), "layer0", AE2MekanismAddons.id(background))
                 .texture("layer1", PORTABLE_CELL_LED);
     }
