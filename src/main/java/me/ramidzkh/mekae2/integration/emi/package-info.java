dfb2377a88b0c0f9bca8ba763cdc2be79f6ac0ea
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-03-07 17:39:28 +0100

    Add support for Mek ingredients when running EMI+JEI

---
+++ src/main/java/me/ramidzkh/mekae2/integration/emi/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package me.ramidzkh.mekae2.integration.emi;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
