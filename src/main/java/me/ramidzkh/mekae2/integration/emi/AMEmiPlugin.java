b01b70f7d82a30941747eeafeda74e2452cd02d2
ramidzkh <ramidzkh@gmail.com>
2024-03-26 01:20:44 +1100

    Native EMI support with Mekanism (#59)

--- src/main/java/me/ramidzkh/mekae2/integration/emi/AMEmiPlugin.java
+++ src/main/java/me/ramidzkh/mekae2/integration/emi/AMEmiPlugin.java
@@ -1,21 +1,16 @@
 package me.ramidzkh.mekae2.integration.emi;
 
-import net.neoforged.fml.ModList;
-
 import dev.emi.emi.api.EmiEntrypoint;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 
+import appeng.api.integrations.emi.EmiStackConverters;
+
 @EmiEntrypoint
 public class AMEmiPlugin implements EmiPlugin {
-    @Override
-    public void register(EmiRegistry emiRegistry) {
-    }
 
-    static {
-        if (ModList.get().isLoaded("jei")) {
-            // We need both EMI and JEI to do anything!
-            MekanismJemiAdapter.init();
-        }
+    @Override
+    public void register(EmiRegistry registry) {
+        EmiStackConverters.register(new ChemicalIngredientConverter());
     }
 }
--- src/main/java/me/ramidzkh/mekae2/integration/emi/ChemicalIngredientConverter.java
+++ src/main/java/me/ramidzkh/mekae2/integration/emi/ChemicalIngredientConverter.java
@@ -1,22 +1,16 @@
 package me.ramidzkh.mekae2.integration.emi;
 
-import com.mojang.logging.LogUtils;
-
 import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
 
 import dev.emi.emi.api.stack.EmiStack;
-import dev.emi.emi.jemi.JemiStack;
-import dev.emi.emi.jemi.JemiUtil;
 import me.ramidzkh.mekae2.ae2.MekanismKey;
+import mekanism.api.IMekanismAccess;
 import mekanism.api.chemical.Chemical;
-import mekanism.api.chemical.ChemicalStack;
 
 import appeng.api.integrations.emi.EmiStackConverter;
 import appeng.api.stacks.GenericStack;
 
 public final class ChemicalIngredientConverter implements EmiStackConverter {
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     @Override
     public Class<?> getKeyType() {
@@ -26,31 +20,25 @@ public final class ChemicalIngredientConverter implements EmiStackConverter {
 
     @Override
     public @Nullable EmiStack toEmiStack(GenericStack stack) {
-        try {
-            if (stack.what() instanceof MekanismKey key) {
-                return JemiUtil.getStack(key.withAmount(Math.max(1, stack.amount())));
-            }
-        } catch (Exception e) { // catch error in case JEMI internals change
-            LOGGER.error("Failed to convert GenericStack to EmiStack", e);
+        if (stack.what() instanceof MekanismKey key) {
+            return IMekanismAccess.INSTANCE.emiHelper().createEmiStack(key.getStack().getType(), stack.amount());
         }
+
         return null;
     }
 
     @Override
     public @Nullable GenericStack toGenericStack(EmiStack stack) {
-        try {
-            if (stack instanceof JemiStack<?> jemiStack) {
-                var ing = jemiStack.ingredient;
-                if (ing instanceof ChemicalStack<?> chemicalStack) {
-                    var mekKey = MekanismKey.of(chemicalStack);
-                    if (mekKey != null) {
-                        return new GenericStack(mekKey, stack.getAmount());
-                    }
-                }
+        var chemical = IMekanismAccess.INSTANCE.emiHelper().asChemicalStack(stack).orElse(null);
+
+        if (chemical != null) {
+            var what = MekanismKey.of(chemical);
+
+            if (what != null) {
+                return new GenericStack(what, stack.getAmount());
             }
-        } catch (Exception e) { // catch error in case JEMI internals change
-            LOGGER.error("Failed to convert EmiStack to GenericStack", e);
         }
+
         return null;
     }
 }
