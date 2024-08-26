15cd8ba779ee07130a2f43cf81452e444d94b99e
mezz <tehgeek@gmail.com>
2024-08-26 11:25:35 +0900

    spotless apply

--- src/main/java/me/ramidzkh/mekae2/integration/jei/AMJeiPlugin.java
+++ src/main/java/me/ramidzkh/mekae2/integration/jei/AMJeiPlugin.java
@@ -1,30 +1,31 @@
 package me.ramidzkh.mekae2.integration.jei;
 
-import me.ramidzkh.mekae2.AppliedMekanistics;
-import mezz.jei.api.IModPlugin;
-import mezz.jei.api.JeiPlugin;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.loading.LoadingModList;
 import net.neoforged.fml.loading.moddiscovery.ModInfo;
 
+import me.ramidzkh.mekae2.AppliedMekanistics;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+
 @JeiPlugin
 public class AMJeiPlugin implements IModPlugin {
-	@Override
-	public ResourceLocation getPluginUid() {
-		return ResourceLocation.fromNamespaceAndPath(AppliedMekanistics.ID, "jei_plugin");
-	}
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ResourceLocation.fromNamespaceAndPath(AppliedMekanistics.ID, "jei_plugin");
+    }
 
-	public AMJeiPlugin() {
-		if (isModLoaded("ae2jeiintegration")) {
-			AE2JeiIntegrationHelper.register();
-		}
-	}
+    public AMJeiPlugin() {
+        if (isModLoaded("ae2jeiintegration")) {
+            AE2JeiIntegrationHelper.register();
+        }
+    }
 
-	private static boolean isModLoaded(String modId) {
-		if (ModList.get() == null) {
-			return LoadingModList.get().getMods().stream().map(ModInfo::getModId).anyMatch(modId::equals);
-		}
-		return ModList.get().isLoaded(modId);
-	}
+    private static boolean isModLoaded(String modId) {
+        if (ModList.get() == null) {
+            return LoadingModList.get().getMods().stream().map(ModInfo::getModId).anyMatch(modId::equals);
+        }
+        return ModList.get().isLoaded(modId);
+    }
 }
--- src/main/java/me/ramidzkh/mekae2/integration/jei/ChemicalIngredientConverter.java
+++ src/main/java/me/ramidzkh/mekae2/integration/jei/ChemicalIngredientConverter.java
@@ -1,43 +1,45 @@
 package me.ramidzkh.mekae2.integration.jei;
 
-import appeng.api.stacks.GenericStack;
+import org.jetbrains.annotations.Nullable;
+
 import me.ramidzkh.mekae2.ae2.MekanismKey;
 import mekanism.api.IMekanismAccess;
 import mekanism.api.chemical.ChemicalStack;
 import mekanism.api.integration.jei.IMekanismJEIHelper;
 import mezz.jei.api.ingredients.IIngredientHelper;
 import mezz.jei.api.ingredients.IIngredientType;
-import org.jetbrains.annotations.Nullable;
 import tamaized.ae2jeiintegration.api.integrations.jei.IngredientConverter;
 
+import appeng.api.stacks.GenericStack;
+
 public class ChemicalIngredientConverter implements IngredientConverter<ChemicalStack> {
-	private final IIngredientType<ChemicalStack> ingredientType;
+    private final IIngredientType<ChemicalStack> ingredientType;
 
-	public ChemicalIngredientConverter() {
-		IMekanismJEIHelper mekJeiHelper = IMekanismAccess.INSTANCE.jeiHelper();
-		IIngredientHelper<ChemicalStack> chemicalStackHelper = mekJeiHelper.getChemicalStackHelper();
-		this.ingredientType = chemicalStackHelper.getIngredientType();
-	}
+    public ChemicalIngredientConverter() {
+        IMekanismJEIHelper mekJeiHelper = IMekanismAccess.INSTANCE.jeiHelper();
+        IIngredientHelper<ChemicalStack> chemicalStackHelper = mekJeiHelper.getChemicalStackHelper();
+        this.ingredientType = chemicalStackHelper.getIngredientType();
+    }
 
-	@Override
-	public IIngredientType<ChemicalStack> getIngredientType() {
-		return ingredientType;
-	}
+    @Override
+    public IIngredientType<ChemicalStack> getIngredientType() {
+        return ingredientType;
+    }
 
-	@Override
-	public @Nullable ChemicalStack getIngredientFromStack(GenericStack genericStack) {
-		if (genericStack.what() instanceof MekanismKey key) {
-			return key.withAmount(genericStack.amount());
-		}
-		return null;
-	}
+    @Override
+    public @Nullable ChemicalStack getIngredientFromStack(GenericStack genericStack) {
+        if (genericStack.what() instanceof MekanismKey key) {
+            return key.withAmount(genericStack.amount());
+        }
+        return null;
+    }
 
-	@Override
-	public @Nullable GenericStack getStackFromIngredient(ChemicalStack chemicalStack) {
-		var what = MekanismKey.of(chemicalStack);
-		if (what == null) {
-			return null;
-		}
-		return new GenericStack(what, chemicalStack.getAmount());
-	}
+    @Override
+    public @Nullable GenericStack getStackFromIngredient(ChemicalStack chemicalStack) {
+        var what = MekanismKey.of(chemicalStack);
+        if (what == null) {
+            return null;
+        }
+        return new GenericStack(what, chemicalStack.getAmount());
+    }
 }
